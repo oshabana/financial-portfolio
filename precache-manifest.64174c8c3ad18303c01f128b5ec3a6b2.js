8acadddb6964420c8b1534b45ea817613d7e5017
oshabana <omarwshabana@gmail.com>
2020-07-19 19:54:04 -0400

    Updates

---
+++ precache-manifest.64174c8c3ad18303c01f128b5ec3a6b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b6813f77b16701de9fad64cc392772",
+    "url": "/financial-portfolio/index.html"
+  },
+  {
+    "revision": "9154a6117fc865506476",
+    "url": "/financial-portfolio/static/css/main.a3fab3b0.chunk.css"
+  },
+  {
+    "revision": "c8bfeb78f4a977694c39",
+    "url": "/financial-portfolio/static/js/2.a647a9e4.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/financial-portfolio/static/js/2.a647a9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9154a6117fc865506476",
+    "url": "/financial-portfolio/static/js/main.4c84ad83.chunk.js"
+  },
+  {
+    "revision": "88f1131c79d167683c88",
+    "url": "/financial-portfolio/static/js/runtime-main.5d55e10b.js"
+  }
+]);
