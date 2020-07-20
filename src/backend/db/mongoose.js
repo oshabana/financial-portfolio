d19767d3ddd9f9680594ece7d52a9716b3b8f4c2
oshabana <omarwshabana@gmail.com>
2020-07-19 22:10:48 -0400

    removed issues... maybe

--- src/backend/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-mongoose.connect('mongodb://127.0.0.1:27017/asset-portfolio', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
